6add036f0505dcfb8f87e48478770ff2f8b17c0f
Viet-Hoa Do <doviethoa@doviethoa.com>
2025-08-05 22:57:04 +0100

    Reset selection mark mode after an action
    * If the editor is in selection mark mode and an action other than
      movement occurs, the editor needs to exit selection mark mode.
    * A movement action will not change the status of selection mark mode.
      `continue_selection_mark_mode` flag is added to allow such actions
      to notify that it does not want to change the status.
    
    Signed-off-by: Viet-Hoa Do <doviethoa@doviethoa.com>

--- crates/editor/src/editor.rs
+++ crates/editor/src/editor.rs
@@ -1161,6 +1161,7 @@ pub struct Editor {
     /// Whether we are temporarily displaying a diff other than git's
     temporary_diff_override: bool,
     selection_mark_mode: bool,
+    continue_selection_mark_mode: bool,
     toggle_fold_multiple_buffers: Task<()>,
     _scroll_cursor_center_top_bottom_task: Task<()>,
     serialize_selections: Task<()>,
@@ -2187,6 +2188,7 @@ impl Editor {
             registered_buffers: HashMap::default(),
             _scroll_cursor_center_top_bottom_task: Task::ready(()),
             selection_mark_mode: false,
+            continue_selection_mark_mode: false,
             toggle_fold_multiple_buffers: Task::ready(()),
             serialize_selections: Task::ready(()),
             serialize_folds: Task::ready(()),
@@ -12327,7 +12329,9 @@ impl Editor {
         self.hide_mouse_cursor(HideMouseCursorOrigin::MovementAction, cx);
         self.change_selections(Default::default(), window, cx, |s| {
             s.move_heads_with(|map, head, _| (movement::left(map, head), SelectionGoal::None));
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn move_right(&mut self, _: &MoveRight, window: &mut Window, cx: &mut Context<Self>) {
@@ -12348,7 +12352,9 @@ impl Editor {
         self.hide_mouse_cursor(HideMouseCursorOrigin::MovementAction, cx);
         self.change_selections(Default::default(), window, cx, |s| {
             s.move_heads_with(|map, head, _| (movement::right(map, head), SelectionGoal::None));
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn move_up(&mut self, _: &MoveUp, window: &mut Window, cx: &mut Context<Self>) {
@@ -12475,7 +12481,9 @@ impl Editor {
             s.move_heads_with(|map, head, goal| {
                 movement::down_by_rows(map, head, action.lines, goal, false, text_layout_details)
             })
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn select_up_by_lines(
@@ -12490,7 +12498,9 @@ impl Editor {
             s.move_heads_with(|map, head, goal| {
                 movement::up_by_rows(map, head, action.lines, goal, false, text_layout_details)
             })
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn select_page_up(
@@ -12511,7 +12521,9 @@ impl Editor {
             s.move_heads_with(|map, head, goal| {
                 movement::up_by_rows(map, head, row_count, goal, false, text_layout_details)
             })
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn move_page_up(
@@ -12578,7 +12590,9 @@ impl Editor {
             s.move_heads_with(|map, head, goal| {
                 movement::up(map, head, goal, false, text_layout_details)
             })
-        })
+        });
+
+        self.continue_selection_mark_mode = true;
     }
 
     pub fn move_down(&mut self, _: &MoveDown, window: &mut Window, cx: &mut Context<Self>) {
@@ -12635,7 +12649,9 @@ impl Editor {
             s.move_heads_with(|map, head, goal| {
                 movement::down_by_rows(map, head, row_count, goal, false, text_layout_details)
             })
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn move_page_down(
@@ -12702,6 +12718,8 @@ impl Editor {
                 movement::down(map, head, goal, false, text_layout_details)
             })
         });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn context_menu_first(
@@ -12828,7 +12846,9 @@ impl Editor {
                     SelectionGoal::None,
                 )
             });
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn select_to_previous_subword_start(
@@ -12845,7 +12865,9 @@ impl Editor {
                     SelectionGoal::None,
                 )
             });
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn delete_to_previous_word_start(
@@ -12933,7 +12955,9 @@ impl Editor {
             s.move_heads_with(|map, head, _| {
                 (movement::next_word_end(map, head), SelectionGoal::None)
             });
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn select_to_next_subword_end(
@@ -12947,7 +12971,9 @@ impl Editor {
             s.move_heads_with(|map, head, _| {
                 (movement::next_subword_end(map, head), SelectionGoal::None)
             });
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn delete_to_next_word_end(
@@ -13036,6 +13062,8 @@ impl Editor {
                 )
             });
         });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn delete_to_beginning_of_line(
@@ -13095,7 +13123,9 @@ impl Editor {
                     SelectionGoal::None,
                 )
             });
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn delete_to_end_of_line(
@@ -13196,7 +13226,9 @@ impl Editor {
                     SelectionGoal::None,
                 )
             });
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn select_to_end_of_paragraph(
@@ -13217,7 +13249,9 @@ impl Editor {
                     SelectionGoal::None,
                 )
             });
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn move_to_start_of_excerpt(
@@ -13338,7 +13372,9 @@ impl Editor {
                     SelectionGoal::None,
                 )
             });
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn select_to_start_of_next_excerpt(
@@ -13359,7 +13395,9 @@ impl Editor {
                     SelectionGoal::None,
                 )
             });
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn select_to_end_of_excerpt(
@@ -13380,7 +13418,9 @@ impl Editor {
                     SelectionGoal::None,
                 )
             });
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn select_to_end_of_previous_excerpt(
@@ -13401,7 +13441,9 @@ impl Editor {
                     SelectionGoal::None,
                 )
             });
-        })
+        });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn move_to_beginning(
@@ -13432,6 +13474,8 @@ impl Editor {
         self.change_selections(Default::default(), window, cx, |s| {
             s.select(vec![selection]);
         });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn move_to_end(&mut self, _: &MoveToEnd, window: &mut Window, cx: &mut Context<Self>) {
@@ -13510,6 +13554,8 @@ impl Editor {
         self.change_selections(Default::default(), window, cx, |s| {
             s.select(vec![selection]);
         });
+
+        self.continue_selection_mark_mode = self.selection_mark_mode;
     }
 
     pub fn select_all(&mut self, _: &SelectAll, window: &mut Window, cx: &mut Context<Self>) {
@@ -17043,7 +17089,22 @@ impl Editor {
             })
         }
         self.selection_mark_mode = true;
+        self.continue_selection_mark_mode = true;
         cx.notify();
+    }
+
+    pub fn reset_mark(&mut self, window: &mut Window, cx: &mut Context<Self>) {
+        if self.selection_mark_mode && !self.continue_selection_mark_mode {
+            self.change_selections(SelectionEffects::no_scroll(), window, cx, |s| {
+                s.move_with(|_, sel| {
+                    sel.collapse_to(sel.head(), SelectionGoal::None);
+                });
+            });
+
+            self.selection_mark_mode = false;
+        }
+
+        self.continue_selection_mark_mode = false;
     }
 
     pub fn swap_selection_ends(
